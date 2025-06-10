885eb6415067f3f3c9f4c9da3d6b5fd4395b4f34
timurislamovai <timur.islamov@mail.ru>
2025-06-11 00:27:41 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
-import os
 import json
+import os
 
 USER_DATA_FILE = "users.json"
 
@@ -14,14 +14,16 @@ def load_user_data(user_id):
     if os.path.exists(USER_DATA_FILE):
         with open(USER_DATA_FILE, "r", encoding="utf-8") as f:
             data = json.load(f)
-            return data.get(user_id, {})
+        return data.get(user_id, {})  # Возвращаем только данные по user_id
     return {}
 
-def save_user_data(user_id, user_entry):
+def save_user_data(user_id, user_data):
     data = {}
     if os.path.exists(USER_DATA_FILE):
         with open(USER_DATA_FILE, "r", encoding="utf-8") as f:
             data = json.load(f)
-    data[user_id] = user_entry
+
+    data[user_id] = user_data  # Обновляем или добавляем пользователя
+
     with open(USER_DATA_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
