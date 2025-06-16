a358fb41349574bbc01f64228570f72edcbc02f8
timurislamovai <timur.islamov@mail.ru>
2025-06-17 00:42:14 +0500

    Update models.py

--- models.py
+++ models.py
@@ -11,3 +11,27 @@ class User(Base):
     free_messages_used = Column(Integer, default=0)
     created_at = Column(DateTime, default=datetime.utcnow)
 
+from sqlalchemy.orm import Session
+
+def get_user_by_telegram_id(db: Session, telegram_id: str):
+    return db.query(User).filter(User.telegram_id == telegram_id).first()
+
+def create_user(db: Session, telegram_id: str):
+    user = User(telegram_id=telegram_id)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def update_user_thread_id(db: Session, user: User, thread_id: str):
+    user.thread_id = thread_id
+    db.commit()
+
+def increment_message_count(db: Session, user: User):
+    user.free_messages_used += 1
+    db.commit()
+
+def reset_user_thread(db: Session, user: User):
+    user.thread_id = None
+    user.free_messages_used = 0
+    db.commit()
