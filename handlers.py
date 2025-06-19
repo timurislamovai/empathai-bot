f767310148fdbdb149baccbe22c239153c3a8bf1
timurislamovai <timur.islamov@mail.ru>
2025-06-19 15:55:19 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -116,7 +116,7 @@ async def handle_update(update: dict):
 
                 bot.send_message(chat_id, message_text, reply_markup=withdraw_button)
                 return
-         return
+                return
         
     except Exception as e:
         print("❌ Ошибка в handle_update:", e)
