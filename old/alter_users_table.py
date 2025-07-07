2099d0e474f3eb9ed99afd6fa9af999731ebf4b6
timurislamovai <timur.islamov@mail.ru>
2025-07-07 19:28:49 +0500

    Delete old directory

--- old/alter_users_table.py
+++
@@ -1,6 +0,0 @@
-from database import engine
-from sqlalchemy import text
-
-with engine.connect() as connection:
-    connection.execute(text("ALTER TABLE users ADD COLUMN IF NOT EXISTS referrer_code TEXT;"))
-    print("✅ Колонка referrer_code добавлена (если её не было).")
