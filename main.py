f61ff71a811e6e199c6fa854b2f7ed88009bd583
timurislamovai <timur.islamov@mail.ru>
2025-07-07 19:07:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import traceback
 from aiogram.types import Update
 from handlers import gptchat  # просто импортировать, запускаться он будет автоматически
 from handlers import admin_handlers_aiogram
+from handlers import menu_handlers
 
 from bot_instance import bot, dp
 
