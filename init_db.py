16076c0671dc26d58d454b17e9038add5aa96e88
timurislamovai <timur.islamov@mail.ru>
2025-07-07 16:32:17 +0500

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,1 +1,10 @@
+from models import Base
+from database import engine
 
+def init_db():
+    print("✅ Creating tables...")
+    Base.metadata.create_all(bind=engine)
+    print("✅ Tables created.")
+
+if __name__ == "__main__":
+    init_db()
