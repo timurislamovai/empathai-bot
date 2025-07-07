954275997920757048806cfe06686a253b8b8388
timurislamovai <timur.islamov@mail.ru>
2025-07-07 18:46:16 +0500

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,21 +1,21 @@
-from telegram import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def main_menu():
     return ReplyKeyboardMarkup(
-        [
-            [KeyboardButton("💳 Купить подписку")],
-            [KeyboardButton("📜 Условия пользования"), KeyboardButton("❓ Гид по боту")],
-            [KeyboardButton("🔄 Сбросить диалог")],
-            [KeyboardButton("👤 Личный кабинет"), KeyboardButton("🤝 Партнёрская программа")]
+        keyboard=[
+            [KeyboardButton(text="💳 Купить подписку")],
+            [KeyboardButton(text="📜 Условия пользования"), KeyboardButton(text="❓ Гид по боту")],
+            [KeyboardButton(text="🔄 Сбросить диалог")],
+            [KeyboardButton(text="👤 Личный кабинет"), KeyboardButton(text="🤝 Партнёрская программа")]
         ],
         resize_keyboard=True
     )
 
 def subscription_plan_keyboard():
     return ReplyKeyboardMarkup(
-        [
-            [KeyboardButton("🗓 Купить на 1 месяц"), KeyboardButton("📅 Купить на 1 год")],
-            [KeyboardButton("🔙 Назад в главное меню")]
+        keyboard=[
+            [KeyboardButton(text="🗓 Купить на 1 месяц"), KeyboardButton(text="📅 Купить на 1 год")],
+            [KeyboardButton(text="🔙 Назад в главное меню")]
         ],
         resize_keyboard=True
     )
