4418d246ba2334381fdc828495de26936daeebfe
timurislamovai <timur.islamov@mail.ru>
2025-07-07 21:50:13 +0500

    Update bot_instance.py

--- bot_instance.py
+++ bot_instance.py
@@ -1,15 +1,5 @@
-# bot_instance.py
-
-import os
 from aiogram import Bot, Dispatcher
-from handlers import menu_handlers, aiogram_handlers
+import os
 
-# Инициализация бота и диспетчера
 bot = Bot(token=os.environ["TELEGRAM_TOKEN"])
 dp = Dispatcher()
-
-# Подключение всех маршрутов
-dp.include_routers(
-    menu_handlers.router,
-    aiogram_handlers.router
-)
