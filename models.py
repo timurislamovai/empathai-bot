5e4eec3c7f8ad81144f6b7f4077610424ed7a90c
timurislamovai <timur.islamov@mail.ru>
2025-07-07 22:33:13 +0500

    Update models.py

--- models.py
+++ models.py
@@ -33,7 +33,7 @@ class User(Base):
     subscription_expires_at = Column(DateTime, nullable=True)
 
 
-def get_user_by_telegram_id(db: Session, telegram_id: str):
+def get_user_by_telegram_id(db: Session, telegram_id: int):  # ✅
     return db.query(User).filter(User.telegram_id == telegram_id).first()
 
 
